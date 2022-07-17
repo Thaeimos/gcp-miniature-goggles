d8181f0e105ce9628410ce2d99d65430e418bd47
Rafa Adorna <rafael.adorna@gmail.com>
2022-07-17 17:35:55 +0200

    Add main code for module cloud-function

---
+++ infra-as-code/modules/cloud-function/variables.tf
@@ -0,0 +1,8 @@
+# terraform/modules/function/variables
+
+variable "project" {}
+variable "gcp_region" {}
+variable "function_name" {}
+variable "entry_point" {}
+variable "function_folder" {}
+variable "runtime" {}
