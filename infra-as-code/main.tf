3279dd597bdeab9cb830b41c8643480f01caeeb5
Rafa Adorna <rafael.adorna@gmail.com>
2022-07-17 17:31:52 +0200

    Instantiate the cloud function

--- infra-as-code/main.tf
+++ infra-as-code/main.tf
@@ -16,7 +16,17 @@ resource "google_storage_bucket" "bucket_data_retention" {
 # We start the App Engine service and create a 'default' version to be used or not later on codes update
 
 resource "google_storage_bucket" "bucket_data_ingestion" {
-  name = "${var.gcp_project}-data-ingestion-${lower(random_string.random.result)}"
+  name          = "${var.gcp_project}-data-ingestion-${lower(random_string.random.result)}"
   location      = var.gcp_region
   force_destroy = true
+}
+
+module "cloud-function" {
+  source          = "./modules/cloud-function"
+  project         = var.gcp_project
+  gcp_region      = var.gcp_region
+  function_name   = "production-app"
+  entry_point     = "hello_world"
+  function_folder = "./cloud-function-producer"
+  runtime         = "python39"
 }
