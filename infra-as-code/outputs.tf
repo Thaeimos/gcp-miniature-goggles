e31187b082153d618b0495d6e6380219fd842f22
Rafa Adorna <rafael.adorna@gmail.com>
2022-07-24 17:51:57 +0200

    Get output from both instantiated functions

--- infra-as-code/outputs.tf
+++ infra-as-code/outputs.tf
@@ -1,3 +1,7 @@
-output "function_url" {
-  value = module.cloud-function.function_url
+output "producer_function_url" {
+  value = module.cloud-function-producer.function_url
+}
+
+output "consumer_function_url" {
+  value = module.cloud-function-consumer.function_url
 }
