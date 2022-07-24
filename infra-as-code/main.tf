13c8d039a1e8d00103aa41d50d066fe4c72ca370
Rafa Adorna <rafael.adorna@gmail.com>
2022-07-24 17:51:39 +0200

    Remove unneeded buckets. Correctly call modules. Create 2 cloud functions

--- infra-as-code/main.tf
+++ infra-as-code/main.tf
@@ -1,31 +1,55 @@
 # Random string for the bucket creation
-
 resource "random_string" "random" {
   length  = 9
   special = false
   lower   = true
 }
 
-resource "google_storage_bucket" "bucket_data_retention" {
-  name = "${var.gcp_project}-data-retention-${lower(random_string.random.result)}"
-  location      = var.gcp_region
-  force_destroy = true
-}
-
-# App Engine for data ingestion section
-# We start the App Engine service and create a 'default' version to be used or not later on codes update
-
-resource "google_storage_bucket" "bucket_data_ingestion" {
-  name          = "${var.gcp_project}-data-ingestion-${lower(random_string.random.result)}"
-  location      = var.gcp_region
-  force_destroy = true
-}
-
-module "cloud-function" {
+# Consumer cloud function
+module "cloud-function-producer" {
   source          = "./modules/cloud-function"
   project         = var.gcp_project
   gcp_region      = var.gcp_region
-  function_name   = "production-app"
+  function_name   = "production-app-producer"
+  entry_point     = "hello_world"
+  function_folder = "./cloud-function-producer"
+  runtime         = "python39"
+}
+
+# Setup scheduler and permissions
+resource "google_service_account" "service_account" {
+  account_id   = "cloud-function-invoker"
+  display_name = "Cloud Function Tutorial Invoker Service Account"
+}
+
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = var.gcp_project
+  region         = var.gcp_region
+  cloud_function = module.cloud-function-producer.function_name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+resource "google_cloud_scheduler_job" "hellow-world-job" {
+  name         = "scheduler-data-ingestion"
+  description  = "Trigger the ${module.cloud-function-producer.function_name} Cloud Function every 10 mins."
+  schedule     = "0/2 * * * *"
+  http_target {
+    http_method = "GET"
+    uri = module.cloud-function-producer.function_url
+    oidc_token {
+      service_account_email = google_service_account.service_account.email
+    }
+  }
+}
+
+# Second instance
+module "cloud-function-consumer" {
+  source          = "./modules/cloud-function"
+  project         = var.gcp_project
+  gcp_region      = var.gcp_region
+  function_name   = "production-app-consumer"
   entry_point     = "hello_world"
   function_folder = "./cloud-function-producer"
   runtime         = "python39"
