69ddc0190537a4831e0b1549903e5e1dcdef3a86
Rafa Adorna <rafael.adorna@gmail.com>
2022-07-24 17:52:07 +0200

    Output name and URL

--- infra-as-code/modules/cloud-function/outputs.tf
+++ infra-as-code/modules/cloud-function/outputs.tf
@@ -1,3 +1,7 @@
 output "function_url" {
   value = google_cloudfunctions_function.function.https_trigger_url
 }
+
+output "function_name" {
+  value = google_cloudfunctions_function.function.name
+}
